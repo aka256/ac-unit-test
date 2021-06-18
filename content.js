9349098d97490195e28d57668feb9d322aae5e9a
aka256 <redmark.2048@gmail.com>
2021-06-18 20:04:52 +0900

    python: 問題ファイルとの分離、resolveにreadlineを渡すよう変更

--- content.js
+++ content.js
@@ -177,13 +177,14 @@ function createPyUnittest(io) {
 `import sys
 from io import StringIO
 import unittest
-
+from import resolve
 
 class TestClass(unittest.TestCase):
     def assertIO(self, input, output):
         stdout, stdin = sys.stdout, sys.stdin
         sys.stdout, sys.stdin = StringIO(), StringIO(input)
-        resolve()
+        s = sys.stdin.readline
+        resolve(s)
         sys.stdout.seek(0)
         out = sys.stdout.read()[:-1]
         sys.stdout, sys.stdin = stdout, stdin
